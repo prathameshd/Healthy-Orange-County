1364c252968fa54c2c0fcfcfe18c653155280ae1
Mirza Ali Beg <mab@localhost.localdomain>
2020-03-25 18:26:00 -0400

    changed db user to 'newuser'

--- controllers/authController.php
+++ controllers/authController.php
@@ -5,7 +5,7 @@ $username = "";
 $email = "";
 $errors = [];
 
-$conn = new mysqli('localhost', 'root', '', 'verifyuser');
+$conn = new mysqli('localhost', 'newuser', 'password', 'verifyuser');
 
 // SIGN UP USER
 if (isset($_POST['signup-btn'])) {
